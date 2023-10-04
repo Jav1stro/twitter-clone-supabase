254339f1e14c05089dac1dcb6009a3bb810895c6
Javier Amarilla <javier.amarilla@casademoneda.gob.ar>
2023-10-04 19:40:25 -0300

    Agrego auth con supabase y conditional rendering de boton de signin-signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthButtonServer } from "./components/auth-button/auth-button-server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: posts } = await supabase.from("posts").select();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola! 📹 
+      Hola! 🇦🇷
+      <AuthButtonServer />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </main>
-  )
+  );
 }
